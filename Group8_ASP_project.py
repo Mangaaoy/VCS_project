8b4905c5c63d2b2ede2eceb86d2bf0726698562d
TristanYeo <tristanyeo123@gmail.com>
2021-08-19 12:38:47 +0800

    hello

--- Group8_ASP_project.py
+++ Group8_ASP_project.py
@@ -1,1 +1,10 @@
-BIJ
+import pandas as pd
+file= pd.read_excel('IMVA.xls',sheet_name='IMVA')
+print(file)
+
+files=file['Periods'].str.split(' ',n=1,expand=True)
+print(files)
+
+print("Summarize the first 100 columns")
+print(file.iloc['Belgium & Luxembourg','Denmark','Finland','France','Germany','Italy','Netherlands','Norway','Rep of Ireland',
+      'Russian Federation','Spain','Sweden','Switzerland','United Kingdom'].describe())
